33aae21109b48180728381583dd272cfff94d873
asendia2025 <aseng@ase-semi.com>
2026-01-14 18:02:42 +0900

    Add files via upload

---
+++ sales_target_data_2026.js
@@ -0,0 +1,101 @@
+/**
+ * 2026년 영업 목표 데이터 (ASM Sales Only)
+ */
+window.salesTarget2026 = [
+  {
+    "year": 2026,
+    "month": 1,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 1708400000
+  },
+
+  {
+    "year": 2026,
+    "month": 2,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 1708400000
+  },
+
+  {
+    "year": 2026,
+    "month": 3,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2277900000
+  },
+
+  {
+    "year": 2026,
+    "month": 4,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2277900000
+  },
+
+  {
+    "year": 2026,
+    "month": 5,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2562600000
+  },
+
+  {
+    "year": 2026,
+    "month": 6,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2562600000
+  },
+
+  {
+    "year": 2026,
+    "month": 7,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2847300000
+  },
+
+  {
+    "year": 2026,
+    "month": 8,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2847300000
+  },
+
+  {
+    "year": 2026,
+    "month": 9,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2847300000
+  },
+
+  {
+    "year": 2026,
+    "month": 10,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2847300000
+  },
+
+  {
+    "year": 2026,
+    "month": 11,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 2135500000
+  },
+
+  {
+    "year": 2026,
+    "month": 12,
+    "customer": "ASM",
+    "category": "Sales",
+    "amount": 1993100000
+  }
+ 
+];
